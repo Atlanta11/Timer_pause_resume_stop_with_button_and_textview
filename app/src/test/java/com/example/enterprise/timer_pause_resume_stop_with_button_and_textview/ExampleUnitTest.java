97317271819a4bc4fae2eb6975a56fb4167ed3b0
Atlanta11 <forever.friends.1980@gmail.com>
2016-09-06 15:55:07 +0200

    Initial commit

---
+++ app/src/test/java/com/example/enterprise/timer_pause_resume_stop_with_button_and_textview/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.enterprise.timer_pause_resume_stop_with_button_and_textview;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
